29eb78ac767b4a5d4fa79fd3324de0eb5f931aef
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-05-26 11:02:11 +0200

    improved opacity changing, added closing of the assistant

--- scripts/assistant.js
+++ scripts/assistant.js
@@ -1,29 +1,60 @@
 document.addEventListener("DOMContentLoaded", _=>{
-    const person = document.querySelector(".person");
+    const iks = document.querySelector(".iks");
+    const assistant = document.querySelector(".assistant");
+    const person = document.querySelector("div.person");
     const pages = document.querySelector(".pages");
     const help = document.querySelector(".help");
     const question = document.querySelector(".question");
 
     setTimeout(_=>{
+        assistant.style.display = "block";
+    }, 9000);
+
+    setTimeout(_=>{ 
         person.style.opacity = "1";
         person.style.transition = "opacity 1s";
         help.style.opacity = "1";
         help.style.transition = "opacity 1s";
+        iks.style.opacity = "1";
+        iks.style.transition = "opacity 1s";
     }, 10000);
 
     person.addEventListener("click", _=>{
-        help.style.display = "none";
+        help.style.opacity = "0";
+        help.style.transition = "opacity 1s";
         pages.style.display = "block";
-        if(pages.style.opacity == "1"){
-            pages.style.opacity = "0";
+        if(help.style.display != "none"){
+            setTimeout(_=>{
+                help.style.display = "none";
+                if(pages.style.opacity == "1"){
+                    pages.style.opacity = "0";
+                }
+                else{
+                    pages.style.opacity = "1"
+                }
+                pages.style.transition = "opacity 1s";
+            },1000);
         }
         else{
-            pages.style.opacity = "1"
+            if(pages.style.opacity == "1"){
+                pages.style.opacity = "0";
+            }
+            else{
+                pages.style.opacity = "1"
+            }
+            pages.style.transition = "opacity 1s";
         }
-        pages.style.transition = "opacity 1s";
     })
 
     question.addEventListener("click", _=>{
         window.open("https://chat.openai.com", "blank");
+    });
+
+    iks.addEventListener("click", _=>{
+        assistant.style.opacity = "0";
+        assistant.style.transition = "opacity 1s";
+        setTimeout(_=>{
+            assistant.style.display = "none";
+        }, 1000);
     })
 })
