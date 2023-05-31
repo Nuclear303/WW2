915d07ed2d511e4edf82d5fdd72b9029fb204202
Nuclear303 <oliwier.kotlowski1@gmail.com>
2023-05-31 22:32:48 +0200

    animating selection, cursors now apply to all elements

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,14 +1,45 @@
-const cursor = localStorage.getItem("cursor");
-const star = document.querySelector("*");
-switch(cursor){
-    case '0':{   
-        star.style.cursor = "pointer !important"
+const cursorNum = localStorage.getItem("cursor");
+const cursor = document.querySelector(".cursors");
+const menu = document.querySelector(".menuSlider");
+const star = document.querySelector("html");
+switch(cursorNum){
+    case '0': default:{   
+        star.style.setProperty("cursor", "pointer, auto", "important");
+        
         break;
     }
     case '1':{
-        console.log("aaaa");
         star.style.setProperty("cursor", "url(./media/bullet.svg), auto", "important");
+        document.querySelectorAll("a, button").forEach(element => {
+        element.style.cursor = "url(./media/bullet.svg), auto";
+        });
         break;
     }
-    
+    case '2':{
+        star.style.setProperty("cursor", "url(./media/bullet2.svg), auto", "important");
+        document.querySelectorAll("a, button").forEach(element => {
+            element.style.cursor = "url(./media/bullet2.svg), auto";
+        });   
+        break;
+    }
+    case '3':{
+        star.style.setProperty("cursor", "url(./media/bomb.svg), auto", "important");
+        star.style.setProperty("cursor", "url(./media/bomb.svg), auto", "important");
+        document.querySelectorAll("a, button").forEach(element => {
+            element.style.cursor = "url(./media/bomb.svg), auto";
+        });
+        break;
+    }  
 }
+
+
+menu.addEventListener("click", _=>{
+    if(localStorage.shown == "1"){
+        cursor.style.animation = "cursorSlideOut 1s forwards";
+        localStorage.shown = "0";
+    }
+    else{
+        cursor.style.animation = "cursorSlideIn 1s forwards";
+        localStorage.shown = "1"
+    }
+})
